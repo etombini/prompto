5f210a970f6e55f5b74855a474354a4ad31ab74d
Elvis <elvis@mapom.me>
2021-03-16 15:33:13 +0100

    the "content" parameter for text parts (and parts using it) now expand environment variables

--- pkg/prompter/part_text.go
+++ pkg/prompter/part_text.go
@@ -1,12 +1,16 @@
 package prompter
 
+import "os"
+
 //Text handles configuration to provide printable prompt information
 type Text struct {
 	PartCommon
 	content string `yaml:"content"`
 }
 
-//NewText returns a Text struct
+//NewText returns a Text struct.
+//The "content" key in the map expands environment variables when Prompt() is called. It is not done at
+//declaration time to allow other components to benefit from this.
 func NewText(param map[string]string) Text {
 	return Text{
 		PartCommon: NewPartCommon(param),
@@ -34,5 +38,6 @@ func (t Text) IsNewline() bool {
 
 //Prompt return the string to be written on the screen and its proper length
 func (t Text) Prompt() (string, int) {
+	t.content = os.ExpandEnv(t.content)
 	return colorStringANSI(t.content, t.fgcolor, t.bgcolor, t.font), RealLen(t.content)
 }
